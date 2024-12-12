200f32c8aa503d1bbe1b2a2672cfebb1fa95c5f4
Javier Guerra <javiguerra144@gmail.com>
2024-12-12 22:49:08 +0100

    chore: wip

---
+++ release.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  branches: ["main"],
+  repositoryUrl: "https://github.com/javiguerra144/tw-presets",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: true,
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "css/*.css", label: "CSS files" },
+          { path: "tailwind-preset.ts", label: "Tailwind preset" },
+        ],
+      },
+    ],
+    "@semantic-release/git",
+  ],
+};
